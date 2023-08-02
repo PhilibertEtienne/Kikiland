7bbcc65218d548e55eb736b72c6ae20636e5f7ac
philibertEtienne <philibert.etienne4@orange.fr>
2023-08-02 17:16:27 +0200

    added upload function

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-
 class AdminController extends AbstractController
 {
     /**
@@ -10,8 +9,22 @@ class AdminController extends AbstractController
      */
     public function index(): string
     {
-            $this->checkLoginStatus();
-            return $this->twig->render('Admin/admin.html.twig');
+        $this->checkLoginStatus();
+
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            // Directory where the uploaded files should be stored
+            $uploadDir = 'assets/Images/' . $_POST['destination'];
+
+            // Make sure the destination directory exists; if not, create it
+            if (!file_exists($uploadDir)) {
+                mkdir($uploadDir, 0777, true);
+            }
+
+            // Move the uploaded file to the destination directory
+            $uploadFile = $uploadDir . '/' . basename($_FILES['avatar']['name']);
+            move_uploaded_file($_FILES['avatar']['tmp_name'], $uploadFile);
+        }
+
+        return $this->twig->render('Admin/admin.html.twig');
     }
-    
 }
--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -11,6 +11,11 @@ class UploadController extends AbstractController
     public function index(): string
     {
             $this->checkLoginStatus();
+            if($_SERVER["REQUEST_METHOD"] === "POST" ){ 
+                $uploadDir = 'public/images/' . $_POST['destination'];
+                $uploadFile = $uploadDir . basename($_FILES['avatar']['name']);
+                move_uploaded_file($_FILES['avatar']['tmp_name'], $uploadFile);
+            }
             return $this->twig->render('Admin/upload.html.twig');
     }
     
