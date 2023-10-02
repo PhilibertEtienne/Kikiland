4334b4a494e33a8c43be37d77cba2291607e394b
philibertEtienne <philibert.etienne4@orange.fr>
2023-10-02 15:13:04 +0200

    made slider infinite WIP :art:

--- assets/scripts/slider.js
+++ assets/scripts/slider.js
@@ -1,63 +1,50 @@
 const slider = document.getElementById("slider");
-const slides = document.querySelectorAll(".slider-image");
+let slides = document.querySelectorAll(".slider-image");
 const sliderPage = document.getElementById("slider-page");
+let slideOnEachSide = 2;
+let currentSlideIndex = 0;
+let lastSlideIndex = slides.length - 1;
 
-//Infinite Slider Logic
-let sliderLength = slides.length;
-function sliding(currentIndex) {
-  let sideSlidesAmount = 2;
-  let nextSlides = [];
-  let prevSlides = [];
+const firstSlideClone = slides[0].cloneNode(true);
+const secondSlideClone = slides[1].cloneNode(true);
+const thirdSlideClone = slides[2].cloneNode(true);
+const fourthSlideClone = slides[3].cloneNode(true);
+const fifthSlideClone = slides[4].cloneNode(true);
+const lastSlideClone = slides[slides.length - 1].cloneNode(true);
+const secondLastSlideClone = slides[slides.length - 2].cloneNode(true);
+const thirdLastSlideClone = slides[slides.length - 3].cloneNode(true);
+const fourthLastSlideClone = slides[slides.length - 4].cloneNode(true);
+const fifthLastSlideClone = slides[slides.length - 5].cloneNode(true);
 
-  function getSliderClone(currentIndex) {
-    nextSlides = [];
-    prevSlides = [];
-
-    for (let i = currentIndex; i < currentIndex + sideSlidesAmount; i++) {
-      nextSlides.push(slides[i + 1]);
-      prevSlides.push(slides[-i - 1]);
-    }
-
-    const cloneNext = document.createDocumentFragment();
-    const clonePrev = document.createDocumentFragment();
-
-    nextSlides.forEach((nextSlide) => {
-      const clone = nextSlide.cloneNode(true);
-      cloneNext.appendChild(clone);
-    });
-
-    prevSlides.forEach((prevSlide) => {
-      const clone = prevSlide.cloneNode(true);
-      clonePrev.appendChild(clone);
-    });
-
-    slider.appendChild(cloneNext);
-    slider.insertBefore(clonePrev);
-  }
-
-  getSliderClone(currentIndex);
-  clg
-}
+slider.appendChild(firstSlideClone);
+slider.appendChild(secondSlideClone);
+slider.appendChild(thirdSlideClone);
+slider.appendChild(fourthSlideClone);
+slider.appendChild(fifthSlideClone);
+slider.insertBefore(fifthLastSlideClone, slides[0]);
+slider.insertBefore(fourthLastSlideClone, slides[0]);
+slider.insertBefore(thirdLastSlideClone, slides[0]);
+slider.insertBefore(secondLastSlideClone, slides[0]);
+slider.insertBefore(lastSlideClone, slides[0]);
+slides = document.querySelectorAll(".slider-image");
 
 // Slide size logic
 function updateSlides() {
   let smallWidth = window.innerWidth / 6;
   let mediumWidth = window.innerWidth / 3;
   let fullWidth = window.innerWidth / 2;
-  let currentIndex = -1;
   slides.forEach((slide,index) => {
     let imagePosition = slide.getBoundingClientRect().x;
     if (
       (imagePosition >= smallWidth && imagePosition < mediumWidth) ||
-      (imagePosition >= fullWidth && imagePosition < 4 * smallWidth)
+      (imagePosition > fullWidth && imagePosition <= 5 * smallWidth)
     ) {
       slide.style.transform = "scale(0.6)";
       slide.style.opacity = "0.6";
     } else if (imagePosition >= mediumWidth && imagePosition <= fullWidth) {
       slide.style.transform = "scale(1)";
       slide.style.opacity = "1";
-      currentIndex = index
-      sliding(currentIndex)
+      currentSlideIndex = index
     } else {
       slide.style.transform = "scale(0.3)";
       slide.style.opacity = "0.3";
@@ -83,33 +70,49 @@ prevbutton.onclick = () => {
   updateSlides();
 };
 
-// Script launch order
-document.addEventListener("DOMContentLoaded", function () {
-  updateSlides();
+// scrollSlides();
 
-  // slides.forEach(slide => {
-  //   slide.classList.add("slide-transition");
-  // });
-
-  window.addEventListener("resize", () => {
-    updateSlides();
-  });
-  window.addEventListener("scroll", () => {
-    updateSlides();
-  });
-  // scrollSlides();
-});
 slider.addEventListener("scroll", () => {
   updateSlides();
 });
 
 slider.addEventListener("wheel", (evt) => {
   evt.preventDefault();
-
+  console.log(currentSlideIndex)
+  console.log(lastSlideIndex)
   const scrollDirection = evt.deltaY > 0 ? 1 : -1;
 
-  slider.scrollLeft +=
+  slider.scrollLeft -=
     (scrollAmountVW * scrollDirection * window.innerWidth) / 100;
+    resetSlider();
+  updateSlides();
 });
 
+// Script launch order
+document.addEventListener("DOMContentLoaded", function () {
+  updateSlides();
 
+  window.addEventListener("resize", () => {
+    updateSlides();
+  });
+  window.addEventListener("scroll", () => {
+    debounce(updateSlides());
+  });
+});
+
+// debounce
+function debounce(func, timeout = 300) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+}
+
+function resetSlider(){
+  if(currentSlideIndex === lastSlideIndex+1){
+    slider.scrollLeft = (-2* scrollAmountVW  * window.innerWidth) / 100 ;
+  }
+}
