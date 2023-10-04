3cb438b83c6633cee46e8ad0f2efe89b31be76e0
philibertEtienne <philibert.etienne4@orange.fr>
2023-10-04 11:58:26 +0200

    infinite :sparkles:

--- assets/scripts/slider.js
+++ assets/scripts/slider.js
@@ -1,6 +1,9 @@
 const slider = document.getElementById("slider");
+const slider2 = document.getElementById("slider2");
 let slides = document.querySelectorAll(".slider-image");
 const sliderPage = document.getElementById("slider-page");
+let translateX = 0;
+let translateX2 = "-100%";
 
 //Slides cloning
 let slideOnEachSide = 2;
@@ -28,41 +31,34 @@ function updateSlides() {
   let mediumWidth = window.innerWidth / 3;
   let fullWidth = window.innerWidth / 2;
   slides.forEach((slide, index) => {
-    let imagePosition = slide.getBoundingClientRect().x;
-    rotationAnimation(slide);
-    if (
-      (imagePosition >= smallWidth && imagePosition < mediumWidth) ||
-      (imagePosition > fullWidth && imagePosition <= 5 * smallWidth)
-    ) {
-      slide.style.transform = "scale(0.6)";
-      slide.style.opacity = "0.6";
-    } else if (imagePosition >= mediumWidth && imagePosition <= fullWidth) {
+    if (index === currentSlideIndex - 1) {
       slide.style.transform = "scale(1)";
       slide.style.opacity = "1";
-    } else {
+    } else if (index < currentSlideIndex - 2 || index > currentSlideIndex) {
       slide.style.transform = "scale(0.3)";
       slide.style.opacity = "0.3";
+    } else {
+      slide.style.transform = "scale(0.6)";
+      slide.style.opacity = "0.6";
     }
   });
 }
 
 // Button sliding logic
-let slidercontainer = document.getElementById("slider-container");
 let nextbutton = document.querySelector(".next");
 let prevbutton = document.querySelector(".prev");
 let scrollAmountVW = 20;
 
 nextbutton.onclick = () => {
-  slider.style.left =
-    parseInt(slider.style.left || 0) - (20 * window.innerWidth) / 100 + "px";
+  translateX -= scrollAmountVW;
+  slider.style.transform = `translateX(${translateX}vw)`;
   currentSlideIndex += 1;
   resetSlider();
   updateSlides();
 };
-
 prevbutton.onclick = () => {
-  slider.style.left =
-    parseInt(slider.style.left || 0) + (20 * window.innerWidth) / 100 + "px";
+  translateX += scrollAmountVW;
+  slider.style.transform = `translateX(${translateX}vw)`;
   currentSlideIndex -= 1;
   resetSlider();
   updateSlides();
@@ -72,13 +68,11 @@ prevbutton.onclick = () => {
 slider.addEventListener("wheel", (evt) => {
   evt.preventDefault();
   const scrollDirection = evt.deltaY > 0 ? 1 : -1;
-  slider.style.left =
-    parseInt(slider.style.left || 0) +
-    (20 * window.innerWidth * scrollDirection) / 100 +
-    "px";
+  translateX += scrollAmountVW * scrollDirection;
+  slider.style.transform = `translateX(${translateX}vw)`;
   currentSlideIndex -= scrollDirection;
   resetSlider();
-  updateSlides();
+  setTimeout(updateSlides(), 0.3);
 });
 
 // Script launch order
@@ -90,26 +84,24 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-
 // slider reset logic
 function resetSlider() {
   if (currentSlideIndex - 1 > lastSlideIndex + slideOnEachSide) {
-    slider.style.left = "0px";
+    translateX = 0;
+    slider.style.transform = `translateX(${translateX}vw)`;
     currentSlideIndex = slideOnEachSide + 1;
-  }
-  if (currentSlideIndex <= slideOnEachSide) {
-    const maxLeftPosition = lastSlideIndex * scrollAmountVW;
-    slider.style.left = -(maxLeftPosition * window.innerWidth) / 100 + "px";
-    currentSlideIndex = lastSlideIndex + slideOnEachSide + 1;
+  } else if (currentSlideIndex <= slideOnEachSide) {
+    translateX = -(lastSlideIndex * scrollAmountVW);
+    slider.style.transform = `translateX( ${translateX}vw)`;
+    currentSlideIndex = lastSlideIndex + (slideOnEachSide + 1);
   }
 }
-
 
 //rotation animation logic
 
 function rotationAnimation(element) {
-  element.classList.remove('animate');
+  element.classList.remove("animate");
   setTimeout(() => {
-    element.classList.add('animate');
+    element.classList.add("animate");
   }, 0);
 }
